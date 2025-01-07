c240c09ec57c758d6f22897f0d2e2d4023584f01
Yahton <yahton@duck.com>
2025-01-07 05:38:54 +0300

    respond only to certain messages

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ logging.basicConfig(
 
 ai_context: ai.Messages = []
 
-async def ai_chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def ai_text_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global ai_context
     response, ai_context = ai.send_text_request(cast(str, cast(Message, update.effective_message).text), ai_context)
     await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=response)
 
-async def voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def ai_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message is None or update.message.voice is None or update.message.voice.mime_type is None:
         return
 
@@ -39,6 +39,24 @@ async def voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response, ai_context = ai.send_text_request(transcription, ai_context)
     await context.bot.send_message(chat_id=cast(Chat, update.effective_chat).id, text=response)
 
+async def is_bot_mentioned(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    if update.message is not None and update.message.text and context.bot.name in [update.message.text[mention.offset:mention.offset + mention.length] for mention in update.message.entities if mention['type'] == 'mention']:
+        return True
+    return False
+
+async def is_reply_to_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    if (message := update.message) and (reply := message.reply_to_message) and (user := reply.from_user) and user.id == cast(Bot, context.bot).id:
+        return True
+    return False
+
+async def ai_text_with_filters(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if await is_reply_to_bot(update, context) or await is_bot_mentioned(update, context):
+        await ai_text_message(update, context)
+
+async def ai_voice_with_filters(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if await is_reply_to_bot(update, context) or await is_bot_mentioned(update, context):
+        await ai_voice_message(update, context)
+
 def run_bot(admin_ids: list[int]) -> None:
     BOT_TOKEN = getenv("BOT_TOKEN")
     if BOT_TOKEN is None:
@@ -52,13 +70,16 @@ def run_bot(admin_ids: list[int]) -> None:
         .token(BOT_TOKEN)\
         .build()
 
-    private_message_handler = MessageHandler(filters.TEXT & filters.Chat(admin_ids), ai_chat)
-    application.add_handler(private_message_handler)
+    private_text_handler = MessageHandler(filters.TEXT & filters.Chat(admin_ids), ai_text_message)
+    application.add_handler(private_text_handler)
 
-    group_message_handler = MessageHandler(filters.TEXT & filters.ChatType.GROUPS, ai_chat)
-    application.add_handler(group_message_handler)
+    private_voice_handler = MessageHandler(filters.VOICE & filters.Chat(admin_ids), ai_voice_message)
+    application.add_handler(private_voice_handler)
+    
+    group_text_handler = MessageHandler(filters.TEXT & filters.ChatType.GROUPS, ai_text_with_filters)
+    application.add_handler(group_text_handler)
 
-    group_voice_handler = MessageHandler(filters.VOICE & filters.ChatType.GROUPS, voice_message)
+    group_voice_handler = MessageHandler(filters.VOICE & filters.ChatType.GROUPS, ai_voice_with_filters)
     application.add_handler(group_voice_handler)
     
     application.run_polling()
